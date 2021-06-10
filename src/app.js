e2148e0917bc0adcb4da5a143b30978d33151379
jrl-mfcgd <Jemy_rose_lacson@manulife.ca>
2021-06-10 13:40:11 +0800

    Completed module

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const { accounts, users, writeJSON } = require("./data");
 
 const app = express();
 
@@ -11,20 +12,6 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true }));
-
-const accountData = fs.readFileSync(
-  path.join(__dirname, "json", "accounts.json"),
-  "utf-8"
-);
-
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(
-  path.join(__dirname, "json", "users.json"),
-  "utf-8"
-);
-
-const users = JSON.parse(userData);
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Account Summary", accounts: accounts });
@@ -62,12 +49,7 @@ app.post("/transfer", (req, res) => {
   const { from, to, amount } = req.body;
   accounts[to].balance += parseInt(amount);
   accounts[from].balance -= parseInt(amount);
-  let accountsJSON = JSON.stringify(accounts, null, 4)
-  fs.writeFileSync(
-    path.join(__dirname, "json", "accounts.json"),
-    accountsJSON,
-    "utf8"
-  );
+  writeJSON();
   res.render("transfer", { message: "Transfer Completed" });
 });
 
@@ -78,12 +60,7 @@ app.get("/payment", (req, res) => {
 app.post("/payment", (req, res) => {
   accounts.credit.balance -= parseInt(req.body.amount);
   accounts.credit.available += parseInt(req.body.amount);
-  let accountsJSON = JSON.stringify(accounts, null, 4)
-  fs.writeFileSync(
-    path.join(__dirname, "json", "accounts.json"),
-    accountsJSON,
-    "utf8"
-  );
+  writeJSON();
   res.render("payment", {
     message: "Payment Successful",
     account: accounts.credit,
