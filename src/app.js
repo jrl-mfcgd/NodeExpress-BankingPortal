0e0ed373209d37f461f6a435f72969cb5c00c863
jrl-mfcgd <Jemy_rose_lacson@manulife.ca>
2021-06-10 17:46:29 +0800

    Completed module

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+
 const { accounts, users, writeJSON } = require("./data");
+const { accountRoutes } = require("./routes/accounts.js");
+const { servicesRoutes } = require("./routes/services.js");
 
 const app = express();
 
@@ -13,57 +16,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/account", accountRoutes);
+
+app.use("/services", servicesRoutes);
+
 app.get("/", (req, res) => {
   res.render("index", { title: "Account Summary", accounts: accounts });
-});
-
-app.get("/savings", (req, res) => {
-  res.render("account", {
-    account: accounts.savings,
-  });
-});
-
-app.get("/checking", (req, res) => {
-  res.render("account", {
-    account: accounts.savings,
-  });
-});
-
-app.get("/credit", (req, res) => {
-  res.render("account", {
-    account: accounts.savings,
-  });
 });
 
 app.get("/profile", (req, res) => {
   res.render("profile", {
     user: users[0],
-  });
-});
-
-app.get("/transfer", (req, res) => {
-  res.render("transfer");
-});
-
-app.post("/transfer", (req, res) => {
-  const { from, to, amount } = req.body;
-  accounts[to].balance += parseInt(amount);
-  accounts[from].balance -= parseInt(amount);
-  writeJSON();
-  res.render("transfer", { message: "Transfer Completed" });
-});
-
-app.get("/payment", (req, res) => {
-  res.render("payment", { account: accounts.credit });
-});
-
-app.post("/payment", (req, res) => {
-  accounts.credit.balance -= parseInt(req.body.amount);
-  accounts.credit.available += parseInt(req.body.amount);
-  writeJSON();
-  res.render("payment", {
-    message: "Payment Successful",
-    account: accounts.credit,
   });
 });
 
---
+++ src/routes/services.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { accounts, writeJSON } = require("../data");
+
+router.get("/transfer", (req, res) => {
+  res.render("transfer");
+});
+
+router.post("/transfer", (req, res) => {
+  const { from, to, amount } = req.body;
+  accounts[to].balance += parseInt(amount);
+  accounts[from].balance -= parseInt(amount);
+  writeJSON();
+  res.render("transfer", { message: "Transfer Completed" });
+});
+
+router.get("/payment", (req, res) => {
+  res.render("payment", { account: accounts.credit });
+});
+
+router.post("/payment", (req, res) => {
+  accounts.credit.balance -= parseInt(req.body.amount);
+  accounts.credit.available += parseInt(req.body.amount);
+  writeJSON();
+  res.render("payment", {
+    message: "Payment Successful",
+    account: accounts.credit,
+  });
+});
+
+module.exports = router;
